fa85bd35aed1d848d1ea9a16698cef6a69d19203
carefree-request-1153 <sgold5757@gmail.com>
2020-09-04 16:41:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,14 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var test = string
-  if test.toUpperCase() === string {
+  if string.toUpperCase() === string {
     return `"YES INDEED!"`
   }
-  elif test.toLowerCase() === string {
+  else if string.toLowerCase() === string {
     return `"I can't hear you!"`
   }
-  elif test === `"I love you, Grandma."`{
+  else if string === `"I love you, Grandma."`{
     return `"I love you, too"`
   }
 }
